e615d62a689a4451f591c5d459da0f24b3359711
Alanda Leote <alandaleote@gmail.com>
2020-08-05 19:12:09 -0300

    add components pageHeader and teacherItem

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './styles.css';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://begmetobuyit.com/application/css/images/noImage.jpg" />
+                <div>
+                    <strong>Nome do Proffy</strong>
+                    <span>Matéria</span>
+                </div>
+            </header>
+
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, vero doloremque ea at velit eum qui perferendis voluptate? 
+                <br/><br/>
+                Tenetur qui earum molestiae cumque nesciunt nisi recusandae eos culpa vitae magnam.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/Hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button>
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
