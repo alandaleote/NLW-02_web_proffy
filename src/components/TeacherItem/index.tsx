8f22958afc0d881d67fb2f82f8eae549a6cb8e22
alandaleote <alandaleote@gmail.com>
2020-08-09 10:12:04 -0300

    update components. add axios. connect to the api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
+import api from '../../services/api';
 import './styles.css';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    name: string;
+    avatar: string;
+    bio: string;
+    whatsapp: string;
+    subject: string;
+    cost: number;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://begmetobuyit.com/application/css/images/noImage.jpg" alt='avatar' />
+                <img src={teacher.avatar} alt='avatar' />
                 <div>
-                    <strong>Nome do Proffy</strong>
-                    <span>Matéria</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, vero doloremque ea at velit eum qui perferendis voluptate? 
-                <br/><br/>
-                Tenetur qui earum molestiae cumque nesciunt nisi recusandae eos culpa vitae magnam.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button>
+                <a target="blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom'
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
@@ -8,16 +9,17 @@ import Select from '../../components/Select';
 import './styles.css'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
+import api from '../../services/api';
 
 function TeacherForm() {
+    const history = useHistory();
+
     const [name, setName] = useState('');
     const [avatar, setAvatar] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
     const [bio, setBio] = useState('');
-
     const [subject, setSubject] = useState('');
     const [cost, setCost] = useState('');
-
     const [scheduleItems, setScheduleItems] = useState([
         { week_day: 0, from: '', to: '' }
     ])
@@ -29,7 +31,6 @@ function TeacherForm() {
         ]);
     }
 
-
     function setScheduleItemValue(position: number, field: string, value: string) {
         const updatedScheduleItems = scheduleItems.map((scheduleItem, index) => {
             if (index === position) {
@@ -38,22 +39,26 @@ function TeacherForm() {
 
             return scheduleItem;
         });
-
         setScheduleItems(updatedScheduleItems);
 
     }
 
     function handleCreateClass(e: FormEvent) {
         e.preventDefault();
-
-        console.log({
+        api.post('classes', {
             name,
             avatar,
             whatsapp,
             bio,
             subject,
-            cost,
-            scheduleItems
+            cost: Number(cost),
+            schedule: scheduleItems
+        }).then(() => {
+            alert('Cadastro realizado com sucesso!');
+
+            history.push('/')
+        }).catch(() => {
+            alert('Erro no cadastro!');
         })
     }
 
